3e2dcd8df497062ad6aeb929e170e2a6756cdcc7
Marcel <marcel.ba@proton.me>
2025-10-17 23:55:02 +0200

    added user input for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -20,17 +21,42 @@ func main() {
 	}
 	defer conn.Close()
 	log.Println("successfully connected to server")
+	gamelogic.PrintServerHelp()
 	newChan, err := conn.Channel()
 	if err != nil {
 		log.Fatalf("unable to create a channel from estableshed connection: %v", err)
 	}
-	err = pubsub.PublishJSON(newChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
-	if err != nil {
-		log.Printf("failed to pause game: %v", err)
+	stopLoop := false
+	for {
+		userInput := gamelogic.GetInput()
+		if len(userInput) == 0 {
+			continue
+		}
+		command := userInput[0]
+		switch command {
+		case "pause":
+			err = pubsub.PublishJSON(newChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+			if err != nil {
+				log.Printf("failed to pause game: %v", err)
+			}
+		case "resume":
+			err = pubsub.PublishJSON(newChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+			if err != nil {
+				log.Printf("failed to pause game: %v", err)
+			}
+		case "quit":
+			log.Println("exiting...")
+			stopLoop = true
+		default:
+			log.Printf("command: %v not found!", command)
+		}
+		if stopLoop {
+			break
+		}
 	}
 	// shutting down the server
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
-	log.Println("shutting down")
+	log.Println("shutting down...")
 }
